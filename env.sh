bd2b56394cf473b117eed11dc25d0caf4d4d7985
Wesley Dias <wesd_444@hotmail.com>
2022-03-18 18:58:49 -0300

    Adiacao de mensagem erro/sucesso ao configurar ambiente

--- env.sh
+++ env.sh
@@ -4,6 +4,15 @@ ENV_PATH='./src/main/resources/'
 ENV_FILE='application.properties'
 ENV_TYPE=${1:-'dev'}
 
-echo -n > $ENV_PATH/application.properties
-cat $ENV_PATH/application-base.properties > $ENV_PATH/$ENV_FILE
-cat $ENV_PATH/application-$ENV_TYPE.properties >> $ENV_PATH/$ENV_FILE
+if [ $# -eq 0 ]; then
+    echo 'using default env (dev)'
+fi
+
+if test -f "$ENV_PATH/application-$ENV_TYPE.properties"; then
+    echo -n > $ENV_PATH/application.properties
+    cat $ENV_PATH/application-base.properties > $ENV_PATH/$ENV_FILE
+    cat $ENV_PATH/application-$ENV_TYPE.properties >> $ENV_PATH/$ENV_FILE
+    echo 'env' $ENV_TYPE': successfully configured!'
+else
+    echo 'env' $ENV_TYPE': error configuring environment!'
+fi
