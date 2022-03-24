d8f0093428e5b871ac4a116d1223496b1b9896dc
Denis Lima <denislima99@gmail.com>
2022-03-23 22:59:37 -0300

    Criando serviços de usuário

--- src/main/java/br/com/fatec/server/controllers/UserController.java
+++ src/main/java/br/com/fatec/server/controllers/UserController.java
@@ -1,13 +1,8 @@
 package br.com.fatec.server.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,69 +15,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fatec.server.dtos.UserDto;
 import br.com.fatec.server.entities.UserEntity;
-import br.com.fatec.server.exceptions.ResourceAlreadyExistsException;
-import br.com.fatec.server.exceptions.ResourceNotFoundException;
-import br.com.fatec.server.mappers.ProjectionMapper;
-import br.com.fatec.server.mappers.UserMapper;
 import br.com.fatec.server.projections.UserProjection;
-import br.com.fatec.server.repositories.UserRepository;
 import br.com.fatec.server.responses.SuccessResponse;
+import br.com.fatec.server.services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    UserMapper mapper;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    UserService userService;
 
     @PostMapping()
     public ResponseEntity<SuccessResponse> create(@RequestBody UserEntity user) {
-        user.setUsePassword(passwordEncoder.encode(user.getUsePassword()));
-        Optional<UserEntity> userExists = userRepository.findByUseEmail(user.getUseEmail());
-        System.out.println(userExists.isEmpty());
-        if (userExists.isPresent()) {
-            throw new ResourceAlreadyExistsException("Email already exists");
-        }
-        UserEntity newUser = userRepository.save(user);
-        SuccessResponse response = new SuccessResponse(ProjectionMapper.convertObject(UserProjection.WithoutPassword.class, newUser));
+        SuccessResponse response = new SuccessResponse(
+                userService.createUser(user, UserProjection.WithoutPassword.class));
         return new ResponseEntity<SuccessResponse>(response, HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<SuccessResponse> getAll(@RequestParam(value = "page", defaultValue = "0") Integer page) {
-        Page<UserProjection.WithoutPassword> user = userRepository.findAllProjectedBy(PageRequest.of(page, 10));
-        SuccessResponse response = new SuccessResponse(user.getContent());
+        SuccessResponse response = new SuccessResponse(userService.getAllUsers(page));
         return new ResponseEntity<SuccessResponse>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{useCod}")
     public ResponseEntity<SuccessResponse> get(@PathVariable Long useCod) {
-        UserProjection.WithoutPassword user = userRepository.findByUseCod(useCod, UserProjection.WithoutPassword.class);
+        UserProjection.WithoutPassword user = userService.getUserById(useCod, UserProjection.WithoutPassword.class);
         SuccessResponse response = new SuccessResponse(user);
         return new ResponseEntity<SuccessResponse>(response, HttpStatus.OK);
     }
 
     @PutMapping("/{useCod}")
     public ResponseEntity<SuccessResponse> update(@PathVariable Long useCod, @RequestBody UserDto newUser) {
-        UserEntity user = userRepository.findByUseCod(useCod);
-        if (user == null) {
-            throw new ResourceNotFoundException("User not found in database");
-        }
-        mapper.updateUserFromDto(newUser, user);
-        UserEntity updatedUser = userRepository.save(user);
-        SuccessResponse response = new SuccessResponse(ProjectionMapper.convertObject(UserProjection.WithoutPassword.class, updatedUser));
+        SuccessResponse response = new SuccessResponse(userService.updateUser(useCod, newUser,
+                UserProjection.WithoutPassword.class));
         return new ResponseEntity<SuccessResponse>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{useCod}")
     public ResponseEntity<SuccessResponse> delete(@PathVariable Long useCod) {
-        UserProjection.WithoutPassword user = userRepository.findByUseCod(useCod, UserProjection.WithoutPassword.class);
-        userRepository.deleteById(useCod);
+        UserProjection.WithoutPassword user = userService.deleteUserById(useCod, UserProjection.WithoutPassword.class);
         SuccessResponse response = new SuccessResponse(user);
         return new ResponseEntity<SuccessResponse>(response, HttpStatus.OK);
     }
